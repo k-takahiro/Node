c0a91434b8c61a1cb08a235f56dac1216eafeb2b
k-takahiro <kono@nippon.co.jp>
2023-07-09 10:39:47 +0900

    ファイル削除のサンプル作成

---
+++ deleteFileSample.js
@@ -0,0 +1,34 @@
+
+// ファイル削除
+// const { unlink } = require('node:fs');
+const fs = require('fs');
+
+const getCommand = () => (  {
+    programName : process.argv.length > 0 ? process.argv[0] : null,
+    scriptName : process.argv.length > 1 ? process.argv[1] : null,
+    args : process.argv.filter( (e,i)=>i>1)
+});
+
+const command = getCommand();
+
+console.log('programName: ' + command.programName);
+console.log('scriptName: ' + command.scriptName);
+console.log('args: ' + command.args);
+
+// 引数で指定されたファイルを削除
+if (Array.isArray(command.args)) {
+    for (let file of command.args) {
+        // 以前はfs.exists()メソッドを使用できましたが、現在は非推奨
+        if (fs.existsSync(file)) {
+            console.log('ファイルあり:' + file);
+            // ファイル削除
+            fs.unlink(file, (err) => {
+                if (err) throw err;
+                console.log('削除成功: ' + file);
+            });    
+        } else {
+            console.log('ファイルなし' + file);
+        }
+    }
+}
+
